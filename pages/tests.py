f02348ae8f1a533954302d32f0b9409a99c538cf
Artem Belyakov <a9113621595@gmail.com>
2022-06-14 03:01:39 +0300

    initial commit

---
+++ pages/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+
+class SimpleTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
+
+# Create your tests here.
---
+++ pages/views.py
@@ -0,0 +1,10 @@
+
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+# Create your views here.
